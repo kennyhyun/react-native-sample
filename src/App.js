91bbc2c0f2412ff9cedf6c353802d0336a0eb1c8
Kenny Yeo <kenny@yeoyou.net>
2019-12-31 12:15:56 +0000

    render react native

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View className="App">
+      <View>
+        <Text>Edit src/App.js and save to reload.</Text>
+        <Text>Learn React</Text>
+      </View>
+    </View>
   );
 }
 
